a931c1c27d1cc04329997c09eda12afb26d31a83
kedhas <kedhas@gmail.com>
2023-02-18 11:20:59 -0500

    added image

--- src/views/LandingPage/LandingPage.tsx
+++ src/views/LandingPage/LandingPage.tsx
@@ -7,15 +7,20 @@ import lessons from '../../data/lessons.json'
 function LandingProps() {
 	const navigate = useNavigate();
 	return (
-		<div className="card-grid">
-			{lessons.map(lesson => (
-				<div className="card" key={lesson.route} onClick={() => {
-					navigate(`/${lesson.route}`)
-				}}>
-					<img src={lesson.image} alt={lesson.title} />
-					<h2>{lesson.title}</h2>
-					<p>{lesson.description}</p>
-				</div>))}
+		<div className='main-page'>
+			<div className='top-image'>
+				<span className='kedhas'>KedHas</span>
+			</div>
+			<div className="card-grid">
+				{lessons.map(lesson => (
+					<div className="card" key={lesson.route} onClick={() => {
+						navigate(`/${lesson.route}`)
+					}}>
+						<img src={lesson.image} alt={lesson.title} />
+						<h2>{lesson.title}</h2>
+						<p>{lesson.description}</p>
+					</div>))}
+			</div>
 		</div>
 	);
 }
